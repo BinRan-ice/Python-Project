a3b34b4f08f1ffa0e90c9273fef4e342efa9f46c
BinRan-ice <lonelinessll@icloud.com>
2022-06-12 18:44:23 +0800

    chap2

---
+++ chap2/demo02_查看python中的保留字.py
@@ -0,0 +1,3 @@
+# coding:utf-8
+import keyword
+print(keyword.kwlist)
---
+++ chap2/demo03_标识符的命名规则.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+my_name_1='努尔夏提'    #my_name_1 就是一个标识符
+
+#1_my_name='努尔夏提'   1_my_name 不符合规范，因为数字开头
+
+My_name_1='python努尔夏提'
+print(my_name_1)
+print(My_name_1)
+
+#True='真'   #不能使用保留字作标识符
+姓名='努尔夏提'   #可以使用中文命名标识符，但是不建议使用
+print(姓名)
