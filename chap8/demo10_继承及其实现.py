cec00e10d7dd22d07a122075e65532c25724630b
BinRan-ice <lonelinessll@icloud.com>
2022-08-21 12:00:25 +0800

    chap8

---
+++ chap8/demo10_继承及其实现.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+class Person(object):   #Person继承object类
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def info(self):
+        print(self.name,self.age)
+
+class Student(Person):
+    def __init__(self,name,age,stu_no):
+        super().__init__(name,age)
+        self.stu_no=stu_no
+
+class Teacher(Person):
+    def __init__(self,name,age,teachofyear):
+        super().__init__(name,age)
+        self.teachofyear=teachofyear
+
+stu=Student('张三',20,1001)
+teacher=Teacher('李四',34,10)
+stu.info()
+teacher.info()
