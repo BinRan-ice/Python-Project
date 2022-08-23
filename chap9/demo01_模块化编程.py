f45586892b124f4ef3ccd2ef3a8f72c0112ffe35
BinRan-ice <lonelinessll@icloud.com>
2022-08-23 23:17:48 +0800

    chap9

---
+++ chap9/demo01_模块化编程.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+def fun():
+    pass
+
+def fun2():
+    pass
+
+class Student:
+    native_place='吉林'   #类属性
+    def eat(self,name,age):
+        self.name=name
+        self.age=age
+    @classmethod
+    def cm(cls):
+        pass
+    @staticmethod
+    def sm():
+        pass
+
+a=10
+b=20
+print(a*b)
---
+++ chap9/demo03_模块的导入.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+import math
+from math import pi
+print(pi)
+print(pow(2,3))
+print(math.pow(2,3))
