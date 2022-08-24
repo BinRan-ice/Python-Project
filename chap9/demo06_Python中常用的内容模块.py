0dc7bc75a2faa39be06135ad2f6e6a5e2d3576c3
BinRan-ice <lonelinessll@icloud.com>
2022-08-24 18:23:04 +0800

    chap9

---
+++ chap9/demo06_Python中常用的内容模块.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+import sys
+import time
+import urllib.request
+print(sys.getsizeof(24))
+print(sys.getsizeof(45))
+print(sys.getsizeof(True))
+print(sys.getsizeof(False))
+print('----------------------------')
+print(time.time())
+print(time.localtime(time.time()))
+print(urllib.request.urlopen('http://www.baidu.com').read())
