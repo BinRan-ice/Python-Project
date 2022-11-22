81700fe059ab5924f5d13ad2e20971a71c29990d
BinRan-ice <lonelinessll@icloud.com>
2022-11-22 12:04:27 +0800

    delete

--- chap1/demo04-使用print输出中文Unicode编码.py
+++
@@ -1,4 +0,0 @@
-print(ord('北')) #北  这个字的编码（数字）21271
-print(ord('京'))
-print('\u5317')
-print('\u4eac')
--- chap1/demo14-实战一.py
+++
@@ -1,3 +0,0 @@
-fp=open('text.txt','w',encoding='utf-8')    #打开文件
-print("人生苦短，我用python",file=fp)  #向文件中写入内容
-fp.close()  #关闭文件
